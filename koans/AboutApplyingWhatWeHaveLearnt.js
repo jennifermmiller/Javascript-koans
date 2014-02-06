2d4c8b9c5d86465d64e024ce97b5aa545fb940e6
Jennifer Miller <jmiller6128@gmail.com>
2014-02-05 23:32:10 -0500

    first commit

---
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -0,0 +1,144 @@
+var _; //globals
+
+describe("About Applying What We Have Learnt", function() {
+
+  var products;
+
+  beforeEach(function () {
+    products = [
+       { name: "Sonoma", ingredients: ["artichoke", "sundried tomatoes", "mushrooms"], containsNuts: false },
+       { name: "Pizza Primavera", ingredients: ["roma", "sundried tomatoes", "goats cheese", "rosemary"], containsNuts: false },
+       { name: "South Of The Border", ingredients: ["black beans", "jalapenos", "mushrooms"], containsNuts: false },
+       { name: "Blue Moon", ingredients: ["blue cheese", "garlic", "walnuts"], containsNuts: true },
+       { name: "Taste Of Athens", ingredients: ["spinach", "kalamata olives", "sesame seeds"], containsNuts: true }
+    ];
+  });
+
+  /*********************************************************************************/
+
+  it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (imperative)", function () {
+
+    var i,j,hasMushrooms, productsICanEat = [];
+    //Other ways to do this: while loop(cleaner syntax), for Each, _.each
+    //Easier to assume somethings false, then prove true (ie has Mushrooms)
+    for (i = 0; i < products.length; i+=1) {
+        if (products[i].containsNuts === false) {
+            hasMushrooms = false;
+            for (j = 0; j < products[i].ingredients.length; j+=1) {
+               if (products[i].ingredients[j] === "mushrooms") {
+                  hasMushrooms = true;
+               }
+            }
+            if (!hasMushrooms) productsICanEat.push(products[i]); //could be better displayed as (hasMushrooms == false)
+        }
+    }
+
+    expect(productsICanEat.length).toBe(1);
+  });
+
+  it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
+
+    var productsICanEat = [];
+
+    /* solve using filter() & all() / any() */  
+    var noNuts = _.filter(products, function(x) {return x.containsNuts == false});
+    var noMush = _.reject(noNuts, function(x) {
+        return _.any(x.ingredients, function(y) {
+          return y === 'mushrooms';
+        });
+      });  
+    productsICanEat.push(noMush);
+   
+    expect(productsICanEat.length).toBe(1);
+  });
+
+  /*********************************************************************************/
+
+  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (imperative)", function () {
+
+    var sum = 0;
+    for(var i=1; i<1000; i+=1) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+
+    expect(sum).toBe(233168);
+  });
+
+  it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
+  /* try chaining range() and reduce() */
+  
+  //first attempt, but koans doesn't support _.union?
+  // var ary = _.union(_.range(0,1000,3),_.range(0,1000,5));
+  // var sum = _.reduce(ary, function(x,y){return x + y});
+
+  //had help w/ this...not sure I really understand using _.chain here
+  var sum = _([_.range(0,1000,3),_.range(0,1000,5)]).chain()
+    .flatten()
+    .reduce(function(x,y){
+      if(y % 15 === 0) {
+        return x + y*.5;
+      } else {
+        return x + y;  
+      };
+    })
+    .value();
+
+    expect(233168).toBe(sum);
+  });
+
+  /*********************************************************************************/
+   it("should count the ingredient occurrence (imperative)", function () {
+    var ingredientCount = { "{ingredient name}": 0 };
+
+    for (i = 0; i < products.length; i+=1) {
+        for (j = 0; j < products[i].ingredients.length; j+=1) {
+            ingredientCount[products[i].ingredients[j]] = (ingredientCount[products[i].ingredients[j]] || 0) + 1;
+        }
+    }
+
+    expect(ingredientCount['mushrooms']).toBe(2);
+  });
+
+  it("should count the ingredient occurrence (functional)", function () {
+    var ingredientCount = { "{ingredient name}": 0 };
+    // pluck instead of map?
+    /* chain() together map(), flatten() and reduce() */
+    _(products).chain() //Chains all below fns
+      .map(function(food){ //pulls out all ingredients from each object
+        return food.ingredients
+      })
+      .flatten() //takes all ingreds and puts into one array
+      .reduce()//not entirely sure what to do here
+      .value()
+
+
+    expect(ingredientCount['mushrooms']).toBe(2);
+  });
+
+  /*********************************************************************************/
+  /* UNCOMMENT FOR EXTRA CREDIT */
+  /*
+  it("should find the largest prime factor of a composite number", function () {
+
+  });
+
+  it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
+
+  });
+
+  it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
+
+
+  });
+
+  it("should find the difference between the sum of the squares and the square of the sums", function () {
+
+  });
+
+  it("should find the 10001st prime", function () {
+
+  });
+  */
+});
